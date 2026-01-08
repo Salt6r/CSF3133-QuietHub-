ba025e896d7dfa6f43c524c2f7e9880960290780
xz-04 <1121iamme@gmail.com>
2026-01-08 14:14:37 +0800

    .

--- README.md
+++
@@ -1,2 +0,0 @@
-# CSF3133-QuietHub-
-This is the project for the music website call QuietHub.
--- script.js
+++
@@ -1,7 +0,0 @@
-const playBtn = document.querySelector(".play");
-let playing = false;
-
-playBtn.addEventListener("click", () => {
-  playing = !playing;
-  playBtn.textContent = playing ? "⏸" : "▶";
-});
